1fca2fea6b048b6999dabd41240b3eef6e18747d
youzhen <yl301114@gmail.com>
2025-01-05 22:58:04 +0000

    修改1

--- public/js/common.js
+++ public/js/common.js
@@ -29,3 +29,48 @@ $(document).click(function (event) {
         $(".signup_input-country_list").slideUp(300);
     }
 });
+
+$(".member_edit").click(function() {
+    $(".member_edit_content").show();
+    $(".member_name_view").hide();
+})
+
+$(".member_edit_content button").click(function() {
+    $(".member_edit_content").hide();
+    $(".member_name_view").show().css("display", "flex");
+})
+
+$(".member_edit_save").click(function() {
+    let username = $(this).parents(".member_edit_content").find("input").val();
+    $(".member_name_view_text").text(username);
+})
+
+$(".member_about_btn-edit").click(function() {
+    $(this).hide();
+    $(".member_about_intro").hide();
+
+    $(".member_about_btn-cancel, .member_about_btn-save, .member_about_textarea").show();
+})
+
+$(".member_about_btn-cancel").click(function() {
+    $(".member_about_intro, .member_about_btn-edit").show();
+    $(".member_about_btn-cancel, .member_about_btn-save, .member_about_textarea").hide();
+})
+
+$(".member_about_btn-save").click(function() {
+    let about = $(".member_about_textarea").val();
+    $(".member_about_intro").text(about);
+    $(".member_about_intro, .member_about_btn-edit").show();
+    $(".member_about_btn-cancel, .member_about_btn-save, .member_about_textarea").hide();
+})
+
+$(".header_more_trigger").click(function() {
+    $(".header_more_list").fadeToggle(300);
+})
+
+$(document).click(function (event) {
+    var headerMore = $(".header_more");
+    if (!headerMore.is(event.target) && headerMore.has(event.target).length === 0) {
+        $(".header_more_list").fadeOut(300);
+    }
+});
